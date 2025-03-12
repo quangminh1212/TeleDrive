868216defdf1b516df8d7a97b270177a0b1ba918
Bach Minh Quang <0947556012abc@gmail.com>
2025-03-12 11:01:10 +0700

    Remove redundant Windows batch scripts and update .gitignore

--- add-nodejs-to-path.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Adding Node.js to PATH...
-setx PATH "%PATH%;C:\Program Files\nodejs" /M
-echo Node.js has been added to PATH.
-echo Please restart your terminal for changes to take effect.
-pause 
