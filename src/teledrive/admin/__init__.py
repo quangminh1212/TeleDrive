55d7750634b495ba4e7aa3f2b7b88a4a3c78b190
Bach Minh Quang <0947556012abc@gmail.com>
2025-07-25 05:35:27 +0700

    fix: comment out imports of non-existent modules in admin/__init__.py

--- src/teledrive/admin/__init__.py
+++ src/teledrive/admin/__init__.py
@@ -9,7 +9,7 @@ from flask import Blueprint
 admin_bp = Blueprint('admin', __name__)
 
 from . import routes
-from . import users
-from . import system
-from . import logs
-from . import settings 
+# from . import users  # Commented out as this module doesn't exist
+# from . import system  # Commented out as this module doesn't exist
+# from . import logs  # Commented out as this module doesn't exist
+# from . import settings  # Commented out as this module doesn't exist 
