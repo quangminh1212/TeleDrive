051c5ca19a8ee1f18ce52b0404d955c88936ca27
Bach Minh Quang <0947556012abc@gmail.com>
2025-07-29 06:32:26 +0700

    Tá»‘i Æ°u dá»± Ã¡n theo chuáº©n quá»‘c táº¿: cáº¥u trÃºc, packaging, tests, vÃ  tÃ i liá»‡u

---
+++ teledrive/__init__.py
@@ -0,0 +1,9 @@
+"""
+TeleDrive - Google Drive-like Telegram File Manager.
+
+Modern web interface for scanning and managing files from Telegram channels.
+"""
+
+__version__ = "0.1.0"
+__author__ = "TeleDrive Team"
+__email__ = "info@teledrive.io"
